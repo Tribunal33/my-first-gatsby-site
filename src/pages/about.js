2588c7cea27f5369f9a94aed86ff859680defb98
Beau Grier <BGrier@pnimedia.com>
2022-04-26 13:40:14 -0700

    some fun changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
             <h1>Hey, Look it's about me</h1>
             <Link to="/">Back to Home</Link>
             <p>This is where I talk about myself in a way that is flattering</p>
-            <p>I built this site myself with Gatsby which I was forced to use</p>
+            <p>I builtttttt this site myself with Gatsby which I was forced to use</p>
             <img src={myFace} alt="This is where my face would go" Style="width: 25%;" />
         </main>
 
