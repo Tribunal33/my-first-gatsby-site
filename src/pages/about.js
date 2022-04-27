46874948415da362e4c6fcb91df05ac37d330b78
Beau Grier <BGrier@pnimedia.com>
2022-04-27 14:38:59 -0700

    adding images with pluging component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,14 @@
 import React from "react"
 import myFace from "../images/myface.jpg"
-import {Link} from "gatsby"
 import Layout from "../components/layout"
 
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Meself">
             <p>This is where I talk about myself in a way that is flattering</p>
-            <p>I builtttttt this site myself with Gatsby which I was forced to use</p>
-            <img src={myFace} alt="This is where my face would go" Style="width: 25%;" />
+            <p>I built this site myself with Gatsby which I was forced to use</p>
+            <img src={myFace} alt="This is where my face would go" Style="width: 25%" />
         </Layout>
-        // <main> 
-        //     <title>About Meself</title>
-        //     <h1>Hey, Look it's about me</h1>
-        //     <Link to="/">Back to Home</Link>
-        //     
-        // </main>
-
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,15 @@
 // Step 1: Import React
 import * as React from 'react'
-import logo from "../images/myphoto.png"
 import Layout from "../components/layout.js"
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <img src={logo} alt="Bhatman" />
+      <StaticImage src="../images/myphoto.png" alt="Bhatman"/>
     </Layout>
-    // <main>
-    //   <title>Home Page</title>
-    //   <h1>Welcome to my Gatsby site!</h1>
-    //   <Link to="/about">About</Link>
-    //   <p>I'm making this by following the Gatsby Tutorial.</p>
-    //   <img src={logo} alt="Bhatman" />
-
-    // </main>
   )
 }
 
